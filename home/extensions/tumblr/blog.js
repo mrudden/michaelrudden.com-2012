4b6e06d821dbac5b100a0fc32aea64bd33e0a805
Michael Rudden <michael.rudden@gmail.com>
2012-05-11 13:36:09 -0400

    transition from tumblr js to json API, with styling

--- home/extensions/tumblr/blog.js
+++ home/extensions/tumblr/blog.js
@@ -11,20 +11,26 @@ for (var i = 0; i<posts.length; i++) {
 		var postBody = post['regular-body'];
  		
 		document.write(
-			'<h3>' + postTitle + '</h3>' + '\n' +
-			'<h4>' + postDate + '</h4>' + '\n' +
-			postBody
+			'<div class="blog-post">' + '\n' +
+			'<h3>' + postDate  + '</h3>' + '\n' +
+			'<h4>' + postTitle + '</h4>' + '\n' +
+			'<h5>&#123;</h5>' + '\n' +
+			postBody + '\n' +
+			'<h5>&#125;</h5>' + '\n' +
+			'</div>'
     		);
 	} else if (postType == 'photo') {
 		var postDate = post['date'];
-		var photoURL = post['photo-url-400'];
+		var photoURLSmall = post['photo-url-250'];
+		var photoURLLarge = post['photo-url-1280'];
 		var photoCaption = post['photo-caption'];
 		
 		document.write(
-			'<h4>' + postDate + '</h4>' + '\n' +
-			'<img src="' + photoURL + '"/>' + '\n' +
-			photoCaption
-			
+			'<div class="blog-photo">' + '\n' +
+			'<h3>' + postDate + '</h3>' + '\n' +
+			'<a href="' + photoURLLarge  + '" target="_blank"><img src="' + photoURLSmall + '"/></a>' + '\n' +
+			photoCaption + '\n' +
+			'</div>'	
     		);
 	} else {
 		document.write('<p>This type of tumblr post not currently supported on this website</p>');
